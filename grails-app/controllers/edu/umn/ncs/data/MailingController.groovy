1adabf12e80c420a960cef0600d98373f5804208
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2011-08-10 21:38:28 +0000

    adding CSV actions for NORC
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-data@550 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/controllers/edu/umn/ncs/data/MailingController.groovy
+++ grails-app/controllers/edu/umn/ncs/data/MailingController.groovy
@@ -133,6 +133,85 @@ class MailingController {
 		}
     }
 
+	// This is a CSV version of the list action
+	def listCsv = {
+		def now = new Date()
+		
+		// List all the batches
+		if ( ! accessService.hasRoleAccess(params.key, request.remoteAddr, 'ROLE_READ_MAILING') ) {
+			response.sendError(403)
+			render "ACCESS DENIED ROLE_READ_MAILING"
+		} else {
+
+			def c = Batch.createCriteria()
+			def batchDirectionInstance = BatchDirection.findByName('outgoing')
+			def instrumentFormatInstance = InstrumentFormat.findByName('first class mail')
+			
+			// first get all NORC instruments
+			def norcInstrumentIds = InstrumentLink.list().collect{it.instrument.id}
+			if (debug) {
+				println "Instruments linked to NORC: "
+				norcInstrumentIds.each{ println "\t${it}" }
+			}
+
+			// finding all mailed batches
+			def batchInstanceList = c.list{
+				and {
+					instruments {
+						instrument {
+							'in'("id", norcInstrumentIds)
+						}
+					}
+					direction {
+						idEq(batchDirectionInstance.id)
+					}
+					format {
+						idEq(instrumentFormatInstance.id)
+					}
+					or {
+						isNotNull("mailDate")
+						isNotNull("addressAndMailingDate")
+						lt("instrumentDate", now)
+					}
+				}
+			}
+
+			def fileName = "ncs-ramsey-mailing-batches.csv" 
+			
+			// Set response header to CSV
+			response.setHeader("Content-disposition", "attachment; filename=\"${fileName}\"");
+			response.contentType = "text/csv"
+	
+			
+			// The column headers
+			def columns = [
+					'batch_id',
+					'primary_instrument_id',
+					'primary_instrument_name',
+					'norc_doc_id',
+					'date_created',
+					'mail_date',
+					'date_on_instrument'
+				]
+			
+			// print them to the response stream
+			response << columns.collect{ '"' + it + '"' }.join(',') + "\n"
+			
+			batchInstanceList.each{ batchInstance ->
+				
+				response << batchInstance.id + ','
+				response << batchInstance.primaryInstrument.id + ','
+				response << '"' + batchInstance.primaryInstrument.name + '",'
+				response << (batchInstance.norcDocId ?: '') + ','
+				response << '"' + (batchInstance.dateCreated?.toString() ?: '') + '",'
+				response << '"' + (batchInstance.mailDate?.toString() ?: '') + '",'
+				response << '"' + (batchInstance.instrumentDate?.toString() ?: '') + '"\n'
+			}
+		}
+		render ""
+	}
+	
+	
 	// the default show action for a mailing/batch
     def show = {
 		def now = new Date()
@@ -252,5 +331,103 @@ class MailingController {
 			}
 		}
     }
+	
+	def showCsv = {
+		def now = new Date()
+		
+		// List all the batches
+		if ( ! accessService.hasRoleAccess(params.key, request.remoteAddr, 'ROLE_READ_MAILING') ) {
+			response.sendError(403)
+			render "ACCESS DENIED ROLE_READ_MAILING"
+		} else {
+
+			def batchInstance = Batch.read(params.id)
+		
+			if (! batchInstance) {
+				response.sendError(404)
+				render "Mailing # ${params.id} Not Found"
+			} else if ( ! batchInstance.norcDocId ) {
+				def writer = new StringWriter()
+				def builder = new MarkupBuilder(writer)
+				builder.message{
+					error "Access Denied to Non-NORC batch."
+					explanation "This batch does not have a matching NORC Doc ID."
+				}
+				render(contentType:"text/xml", text: writer.toString() )
+				return
+			} else {
+	
+				def fileName = "ncs-ramsey-mailing-batch-${batchInstance.id}.csv"
+				
+				// Set response header to CSV
+				response.setHeader("Content-disposition", "attachment; filename=\"${fileName}\"");
+				response.contentType = "text/csv"
+		
+				
+				// The column headers
+				def columns = [
+						'batch_id',
+						'primary_instrument_id',
+						'primary_instrument_name',
+						'norc_doc_id',
+						'date_created',
+						'mail_date',
+						'date_on_instrument',
+						'tracked_item_id',
+						'norc_su_id',
+						'dwelling_unit_id',
+						'person_id',
+						'address_id',
+						'street_address'
+					]
+				
+				// print them to the response stream
+				response << columns.collect{ '"' + it + '"' }.join(',') + "\n"
+				
+				
+				def rowPrefix = ""
+
+				rowPrefix += batchInstance.id + ','
+				rowPrefix += batchInstance.primaryInstrument.id + ','
+				rowPrefix += '"' + batchInstance.primaryInstrument.name + '",'
+				rowPrefix += (batchInstance.norcDocId ?: '') + ','
+				rowPrefix += '"' + (batchInstance.dateCreated?.toString() ?: '') + '",'
+				rowPrefix += '"' + (batchInstance.mailDate?.toString() ?: '') + '",'
+				rowPrefix += '"' + (batchInstance.instrumentDate?.toString() ?: '') + '",'
+
+				if (debug) {
+					println "Row Prefix:\n${rowPrefix}"
+					
+				}
+				
+				batchInstance.items.each{ trackedItemInstance ->
+					
+					response << rowPrefix
+					
+					response << trackedItemInstance.id + ','
+					if (trackedItemInstance.person) {
+						response << '"' + trackedItemInstance.person.norcSuId + '",'
+						response << ','
+						response << trackedItemInstance.person.id + ','
+						response << trackedItemInstance.person.primaryAddress?.id + ','
+						response << '"' + trackedItemInstance.person.primaryAddress?.address + '"'
+						
+					} else if (trackedItemInstance.dwellingUnit) {
+						response << '"' + trackedItemInstance.dwellingUnit.norcSuId + '",'
+						response << trackedItemInstance.dwellingUnit.id + ','
+						response << ','
+						response << trackedItemInstance.dwellingUnit.address.id + ','
+						response << '"' + trackedItemInstance.dwellingUnit.address.address + '"'
+					} else {
+						response << ',,,,'
+					}
+					
+					response << '\n'
+					
+				}
+			}
+			render ""
+		}
+	}
 }
 
