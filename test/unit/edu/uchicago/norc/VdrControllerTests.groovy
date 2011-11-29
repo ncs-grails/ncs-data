a0b27f71276676c44552122ff572efdf45bf2a8e
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2011-11-29 16:34:54 +0000

    adding VDR upload controller
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-data@770 59f948a9-288f-427a-98b3-c46ff3be0d89

---
+++ test/unit/edu/uchicago/norc/VdrControllerTests.groovy
@@ -0,0 +1,17 @@
+package edu.uchicago.norc
+
+import grails.test.*
+
+class VdrControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
